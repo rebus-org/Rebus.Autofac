0964e6479599d4cc39dbb4c866c40aecb96c63a1
Mogens Heller Grabe <mookid8000@gmail.com>
2021-07-06 08:33:55 +0200

    add test to try to reproduce this issue

---
+++ AdditionalHandlerAssembly/ControllerChangeMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Rebus.Handlers;
+#pragma warning disable 1998
+
+namespace AdditionalHandlerAssembly
+{
+    public class ControllerChangeMessageHandler : IHandleMessages<ControllerChangeMessage>
+    {
+        readonly ConcurrentQueue<ControllerChangeMessage> _handledMessages;
+
+        public ControllerChangeMessageHandler(ConcurrentQueue<ControllerChangeMessage> handledMessages) => _handledMessages = handledMessages ?? throw new ArgumentNullException(nameof(handledMessages));
+
+        public async Task Handle(ControllerChangeMessage message) => _handledMessages.Enqueue(message);
+    }
+
+    public class ControllerChangeMessage { }
+}
---
+++ Rebus.Autofac.Tests/Bugs/ReproduceDoubleDispatchIssue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using AdditionalHandlerAssembly;
+using Autofac;
+using NUnit.Framework;
+using Rebus.Bus;
+using Rebus.Config;
+using Rebus.Persistence.InMem;
+using Rebus.Retry.Simple;
+using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Extensions;
+using Rebus.Transport.InMem;
+
+namespace Rebus.Autofac.Tests.Bugs
+{
+    [TestFixture]
+    public class ReproduceDoubleDispatchIssue : FixtureBase
+    {
+        [Test]
+        [Description("Tried to reproduce issue reported: Messages should apparently be dispatched twice. Sp far, reproduction has not been not successful.")]
+        public async Task ItWorks()
+        {
+            var controllerChangeMessages = new ConcurrentQueue<ControllerChangeMessage>();
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterRebus((configurer, _) =>
+            {
+                var queueName = "queue";
+
+                configurer
+                    .Logging(l => l.ColoredConsole())
+                    .Transport(x => x.UseInMemoryTransport(new InMemNetwork(true), queueName))
+                    .Subscriptions(s => s.StoreInMemory(new InMemorySubscriberStore()))
+                    .Options(x =>
+                    {
+                        x.SetMaxParallelism(1);
+                        x.SimpleRetryStrategy(maxDeliveryAttempts: int.MaxValue);
+                    });
+
+                return configurer;
+            });
+
+            builder.RegisterHandlersFromAssemblyOf<ControllerChangeMessageHandler>();
+
+            builder.RegisterInstance(controllerChangeMessages).AsSelf();
+
+            //builder.RegisterBuildCallback(c =>
+            //{
+            //    var bus = c.Resolve<IBus>();
+            //    var messageTypes = AssemblyHelpers.GetImplementations(typeof(IHandleMessages))
+            //        .SelectMany(x => x.GetInterfaces())
+            //        .Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IHandleMessages<>))
+            //        .SelectMany(x => x.GetGenericArguments())
+            //        .Where(x => typeof(RebusMessage).IsAssignableFrom(x))
+            //        .ToList();
+
+            //    foreach (var messageType in messageTypes)
+            //    {
+            //        bus.Subscribe(messageType).GetAwaiter().GetResult();
+            //    }
+            //});
+
+            await using var container = builder.Build();
+
+            var bus = container.Resolve<IBus>();
+
+            await bus.SendLocal(new ControllerChangeMessage());
+
+            // wait for at least one message to arrive
+            await controllerChangeMessages.WaitUntil(q => q.Count >= 1);
+
+            // wait additional time to be sure all messages have been processed
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            // even at this point, the handler should only have been called once!
+            Assert.That(controllerChangeMessages.Count, Is.EqualTo(1));
+        }
+    }
+}
